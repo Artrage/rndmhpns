444a2039f00c30b12bb3b1ed375151b42c90e8f4
Art <art.balenok@gmail.com>
2020-05-09 14:16:37 +0000

    added emailjs api, not working

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,17 @@
+function sendMail(contactForm) {
+    emailjs.send("user_Iqh4xtz1OZqOEvdEiTLb5", "test_template", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "from_subject": contactForm.subject.value,
+        "from_usermessage": contactForm.usermessage.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
+}
