9d206877ccc0c4cff54b513f1f0e01b029fe8008
Art <art.balenok@gmail.com>
2020-06-11 12:20:06 +0000

    clean-up continues

--- assets/js/ppbtn.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $("#ppl ").hover(
-        function () {
-            $(this).attr(url"./assets/images/paypal-button-orng.png");
-        },
-        function () {
-            $(this).attr("src", "./assets/images/paypal-donate.png");
-        });
-});
